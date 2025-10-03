85e68e54f663cffb00921e8ad7e212f5ec44b6b6
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-03 22:21:32 +0530

    script

--- 10-condition.sh
+++ 10-condition.sh
@@ -3,9 +3,8 @@
 echo "please enter the number"
 
 read  NUMBER
-if [ (("${NUMBER}" % 2)) -eq 0 ]; then
+if [ (($NUMBER % 2)) -eq 0 ]; then
     echo "Entered number "${NUMBER} is "EVEN""
 else
     echo "Entered number "${NUMBER} is "ODD""
 fi 
-       
