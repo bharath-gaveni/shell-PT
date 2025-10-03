f73794a2dbb462fcdaedc8f3a85c2cd0014485fc
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-03 22:10:06 +0530

    script

---
+++ 09-condition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# write number lessthan 10
+
+number=$1
+
+if [ "${number}" -le 10 ]; then
+    echo "Entered number is less than 10"
+elif [ "${number}" -eq 10 ]; then
+    echo "Entered number is exactly 10"
+else
+    echo "Entered number is greater than 10"
+fi    
