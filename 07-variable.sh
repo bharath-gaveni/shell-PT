6acf43ec91c28a69618cea13128d45cbfc0cc2f7
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-03 21:49:57 +0530

    script

---
+++ 07-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "number of varibales passed to the script(count of variables): $#"
+echo "number of varaibales passed to the script(what are varaibles means names): $@"
+echo "current/present working directory: $PWD"
+echo "Home of directory of the script: $HOME"
+echo "who is the user running the script:$USER"
+echo "Process instance id of the script: $$"
+echo "Process instance of the last executed command in background: $!"
