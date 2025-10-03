9311b2cbef3d726d0d1554ebe091bac893c4db1c
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-03 21:42:43 +0530

    script

---
+++ 06-variable.sh
@@ -0,0 +1,9 @@
+#!/bin.bash
+
+start_time=$(date +%s)
+echo "please wait for 10 seconds"
+sleep 10
+echo "Your time 10 seconds is completed"
+end_time=(date +%s)
+TOTAL_TIME=$(($end_time-$start_time))
+echo "Total time takem to execute script is: $Total_TIME"
