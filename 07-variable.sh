f0aad9b93328161729f3b338268212865371f2a5
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-03 21:50:51 +0530

    script

--- 07-variable.sh
+++ 07-variable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "number of varibales passed to the script(count of variables): $#"
-echo "number of varaibales passed to the script(what are varaibles means names): $@"
+echo "All variables passed to the script(what are varaibles means names): $@"
 echo "current/present working directory: $PWD"
 echo "Home of directory of the script: $HOME"
 echo "who is the user running the script:$USER"
