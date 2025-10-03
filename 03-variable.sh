546a43319f8199f10c123f520c62dc05dc225110
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-03 21:27:26 +0530

    script

--- 03-variable.sh
+++ 03-variable.sh
@@ -5,3 +5,4 @@ echo "$person1: Hello $person2 how are you"
 echo "$person2: Hello $person1 I am fine"
 echo "$person1: I saw at stadium" 
 echo "$person2: I went to play cricket"
+
---
+++ 04-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "please enter your password I didnt share to everyone its completely secure"
+read PASSWORD
+echo "password entered is: $password"
+
