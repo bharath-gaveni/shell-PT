1906f3156e2f71548bfbf30ad3f15d00af9f8384
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-03 21:17:56 +0530

    script

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=Bujji
+person2=Ramana
+
+echo "Person1: Hello Person2 how are you"
+echo "Person2: Hello Person1 I am fine"
+echo "Person1: I saw at stadium" 
+echo "Person2: I went to play cricket"
